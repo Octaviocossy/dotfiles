c95cd3879b5fa23c9dc7b865a322fedc43d7232a
OctavioCossy <octaviocossytorquati@gmail.com>
2022-10-29 14:37:32 +0000

    Change side of file explorer

--- .config/nvim/after/plugin/nvim-tree.lua
+++ .config/nvim/after/plugin/nvim-tree.lua
@@ -1,30 +1,33 @@
-local status_ok, nvimtree = pcall(require, 'nvim-tree')
+local status_ok, nvimtree = pcall(require, "nvim-tree")
 
-if not status_ok then return end
+if not status_ok then
+	return
+end
 
 nvimtree.setup({
-  sort_by = "case_sensitive",
-  view = {
-    adaptive_size = true,
-    mappings = {
-      list = {
-        { key = "u", action = "dir_up" },
-      },
-    },
-  },
-  renderer = {
-    group_empty = true,
-  },
-  filters = {
-    dotfiles = false,
-  },
-  actions = {
-    open_file = {
-      -- quit_on_open = true,
-    },
-  },
+	sort_by = "case_sensitive",
+	view = {
+		adaptive_size = true,
+		side = "right",
+		mappings = {
+			list = {
+				{ key = "u", action = "dir_up" },
+			},
+		},
+	},
+	renderer = {
+		group_empty = true,
+	},
+	filters = {
+		dotfiles = false,
+	},
+	actions = {
+		open_file = {
+			-- quit_on_open = true,
+		},
+	},
 })
 
 -- Keymap
 
-vim.keymap.set('n', '<Leader>e', ':NvimTreeFocus<Return>')
+vim.keymap.set("n", "<Leader>e", ":NvimTreeFocus<Return>")
