a541a92ebdf0f635015dfb93ca8132aa953f2e38
Octavio Cossy Torquati <octaviocossytorquati@gmail.com>
2023-01-08 17:32:55 -0300

    Update neovim colorscheme

--- .config/nvim/lua/plugins/packer.lua
+++ .config/nvim/lua/plugins/packer.lua
@@ -16,7 +16,7 @@ return require("packer").startup(function()
   use("nvim-lua/plenary.nvim")
 
   -- Colorscheme
-  use('Mofiqul/vscode.nvim')
+  use("folke/tokyonight.nvim")
 
   -- File explorer
   use({
